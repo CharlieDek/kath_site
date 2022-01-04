518afe6c40b6b71ff66da44c9c12ebab0f381f4f
Charlie Dektar <chaz@mac-mini.myfiosgateway.com>
2022-01-03 20:43:55 -0500

    slightly less crazy small

--- js/kath.js
+++ js/kath.js
@@ -2,7 +2,7 @@ const c_COLOR_PAIRS = [
   {"background": "#224fb1", "color": "#c1ffff"},
   {"background": "#fffd97", "color": "#7d70d5"},
   {"background": "#f64042", "color": "#473334"},
-  {"background": "#fffe04", "color": "#fbc03f"},
+  {"background": "#fffe04", "color": "#faaf00"},
   {"background": "#5dd2f0", "color": "#703d54"},
   {"background": "#f77e27", "color": "#2d3738"},
   {"background": "#fcdc2b", "color": "#f53030"},
@@ -11,7 +11,7 @@ const c_COLOR_PAIRS = [
   
   {"background": "#c1ffff", "color": "#224fb1"},
   {"background": "#7d70d5", "color": "#fffd97"},
-  {"background": "#fbc03f", "color": "#fffe04"},
+  {"background": "#faaf00", "color": "#fffe04"},
   {"background": "#703d54", "color": "#5dd2f0"},
   {"background": "#f53030", "color": "#fcdc2b"},
   {"background": "#3d6a3e", "color": "#83c14a"},
@@ -61,7 +61,124 @@ $(".contentLink").hover(function() {
 
 });
 
-particlesJS('particles-js', {
+
+
+if ($(window).width() < 600) {
+
+particlesJS('particles-js', { // small window setting
+    "particles": {
+      "number": {
+        "value": 8,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+        "src": "js/img/fish.png", // http://pngimg.com/uploads/waffle/waffle_PNG49.png
+          "width": 130,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 50,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.7,
+        "direction": "left",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 45.377304642527047,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+}  // end small window setting
+else { // large window setting
+
+  particlesJS('particles-js', {
     "particles": {
       "number": {
         "value": 20,
@@ -171,6 +288,7 @@ particlesJS('particles-js', {
     },
     "retina_detect": true
   });
+}  // end large window
 
 
 setRandomColors();
